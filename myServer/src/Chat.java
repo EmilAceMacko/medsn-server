91c1158e9465ad767eb8b6c842bf7b29a8b0173a
ValdemarBirkegaardPetersen <valdemarlpt@gmail.com>
2020-10-29 13:44:56 +0100

    Further updating the scanThread Thread class.

--- myServer/src/Chat.java
+++ myServer/src/Chat.java
@@ -1,11 +1,26 @@
 package src;
 
+import java.util.Scanner;
+
 public class Chat {
     MEDSN_Server owner;
     Thread scanThread;
 
     public Chat (MEDSN_Server owner) {
         scanThread = new Thread () {
+            private Chat owner;
+            private String input;
+            public Boolean scanning;
+
+            private Scanner scan = new Scanner(System.in);
+
+            public void run() {
+
+            }
+
+            public void writeChat (String msg) {
+
+            }
 
         };
     }
