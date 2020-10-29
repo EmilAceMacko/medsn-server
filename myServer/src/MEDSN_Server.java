4853b86e4f113905d99da476614a0d2336537efd
ValdemarBirkegaardPetersen <valdemarlpt@gmail.com>
2020-10-29 13:19:13 +0100

    Added additional methods and ArrayLists

--- myServer/src/MEDSN_Server.java
+++ myServer/src/MEDSN_Server.java
@@ -1,10 +1,17 @@
 //Valdemar
 package src;
+import java.util.ArrayList;
 
 public class MEDSN_Server implements Constants {
     private short state;
     private Chat chat;
     private Client_Manager clientMgr;
+    private int maxClients;
+    private int chatLength;
+
+    public ArrayList<String> banNameList = new ArrayList<String>();
+    public  ArrayList<String> banAdressList = new ArrayList<String>();
+
 
     public static void main(String[] args) {}
 
@@ -14,5 +21,21 @@ public class MEDSN_Server implements Constants {
 
     public short getState () {
         return state;
+    }
+
+    public void setMaxClients(int _MaxClients) {
+        this.maxClients = _MaxClients;
+    }
+
+    public int getMaxClients () {
+        return maxClients;
+    }
+
+    public void setChatLength (int _chatLength) {
+        this.chatLength = _chatLength;
+    }
+
+    public int getChatLength () {
+        return chatLength;
     }
 }
