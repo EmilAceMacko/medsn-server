a2322cf1cc43a5d8e51814d32ed559c8ae497e67
Semi Seitovski <Semoo@live.dk>
2020-10-30 11:53:00 +0100

    Client Class update

--- myServer/src/Client.java
+++ myServer/src/Client.java
@@ -2,22 +2,48 @@ package src;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Client {
     private Client_Manager owner;
-    private String username;
-    private String address;
-    private Socket clientSocket;
+    public String username;
+    public String address;
+    public Socket clientSocket;
     private DataInputStream in;
     private DataOutputStream out;
+    boolean listening;
+    boolean admin;
 
-    public Client(Socket newClientSocket, Client_Manager newOwner) {
+    //Constructor
+    public Client(Socket newClientSocket, String newUsername, Client_Manager newOwner) {
         this.clientSocket = newClientSocket;
+        this.username = newUsername;
         this.owner = newOwner;
+        address = clientSocket.getRemoteSocketAddress().toString();
     }
 
-    public <type> void Method () {
+    public void run() {
+        while (isListening()) {
+
+        }
     }
 
+    //Booleans are listed below
+    public boolean isListening() {
+        return listening;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    //Setters are listed below
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public void setListening(Boolean listening) {
+        this.listening = listening;
+    }
 }
